9c4995f6bd75f3675236983829e66babebd4ab05
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2023-10-14 01:41:59 -0300

    doc: swagger documentation

--- src/main/java/br/com/gilbertomoj/todolist/controller/MainController.java
+++ src/main/java/br/com/gilbertomoj/todolist/controller/MainController.java
@@ -3,13 +3,17 @@ package br.com.gilbertomoj.todolist.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.http.HttpClient;
 
 @RestController
-@RequestMapping("/hello")
-public class HelloController {
+@RequestMapping("/")
+public class MainController {
 
     @GetMapping
-    public String message(){
-        return "Olá22";
+    public RedirectView message(){
+
+        return new RedirectView("http://localhost:8080/swagger-ui/index.html");
     }
 }
